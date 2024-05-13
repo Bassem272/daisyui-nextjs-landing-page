a58ed96d3fac45b50ed4e7647aa2b4a76b916d67
bassem <bassemgog@gmail.com>
2024-05-13 16:31:21 +0300

    form adding and learning

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 function Hero(){
     return(
-        <div className="hero py-12 bg-gradient-to-t from-blue-500 to-purple-700">
+        <div  className="hero py-12 bg-gradient-to-t from-blue-500 to-purple-700">
             <div className="hero-content md:px-0 px-4 max-w-6xl flex-col lg:flex-row-reverse">
                 <img src="https://plus.unsplash.com/premium_photo-1681319553238-9860299dfb0f?auto=format&fit=crop&q=80&w=2831&ixlib=rb-4.0.3" className="max-w-sm  h-80 object-cover rounded-lg shadow-2xl" />
                 <div>
---
+++ src/pages/form.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Form() {
+    const [inputs, setInputs] = useState({});
+    const [errors , setErrors] = useState({});
+
+  
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      let error = '';
+
+     if(name === 'username'){
+        if(value.length < 4){
+            error = 'Username must be at least 4 characters long'
+        }else if (!value){
+            error = 'Username is required'
+        }
+    }
+    if( name === 'age'){
+            if(!value){
+                error = 'age must contain a value'
+            }else if ( parseInt(value) <= 5){
+                error = 'age is not enough'
+            }
+        }
+
+        setErrors(errors => ({...errors, [name]: error}))
+        // console.log(errors)
+        
+            setInputs(inputs => ({...inputs, [name]: value}))
+        
+     }
+
+
+    
+     
+     const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(Object.values(errors).every(error => !error ) ){
+            alert("form submitted")
+            alert(inputs);
+            console.log(inputs)
+        }else{
+            alert(`{"please fill the form correctly" ${errors.age} and ${errors.username}`)
+        }
+        
+        console.log(errors)
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+        <input 
+          type="text" 
+          name="username" 
+          value={inputs.username || ""} 
+          onChange={handleChange}
+          />
+        {errors.username && <span style={{ color: 'red' }}>{errors.username}</span>}
+        </label>
+        <label>Enter your age:
+          <input 
+            type="number" 
+            name="age" 
+            value={inputs.age || ""} 
+            onChange={handleChange}
+            />
+            {errors.age && <span style={{ color: 'red' }}>{errors.age}</span>}
+          </label>
+          <input type="submit" className='btn'/>
+      </form>
+    )
+}
+  
+export default Form
