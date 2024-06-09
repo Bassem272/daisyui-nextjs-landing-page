dfc12946d81975351f7ee430732e709a537cc437
bassem <bassem.gehad95@yahoo.com>
2024-06-09 20:13:33 +0300

    adding edits for the form

--- src/components/common/SignUpBody.js
+++ src/components/common/SignUpBody.js
@@ -10,6 +10,8 @@ import { SIGN_UP_IMAGES } from "@/utils/globalConstantUtil";
 import { ModalWrapper } from "@/components/common/ModalWrapper";
 import { useRouter } from "next/navigation";
 import * as Yup from 'yup';
+import PhoneInput from 'react-phone-input-2';
+
 
 function SignUpBody({ closeModal, extraObject }) {
   
@@ -307,7 +309,7 @@ useEffect(() => {
           </div>
         </div>
        
-        <div className="md:p-10 pb-12">
+        <div className="md:p-10 pb-12 bg-red-300">
           <form onSubmit={(e) => submitForm(e)}>
             <div className="mb-4">
               {!isOtpSent && (
@@ -392,18 +394,19 @@ useEffect(() => {
                   </div> */}
 
                                {/* Email */}
-        <label className="w-full">
+        <label className="w-full bg-blue-300">
           <div className="label">
-            <span className="label-text">What is your email?</span>
+            <span className="label-text ml-9">What is your email?</span>
           </div>
-          <label className="input input-bordered input-accent flex items-center gap-2">
+          <label className="input input-bordered input-accent 
+          w-5/6 flex items-center gap-2 mx-auto">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
               <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
             <input
               type="text"
-              className="grow"
+              className="grow bg-red-300"
               value={loginObj.email}
               placeholder={"Ex- name@gmail.com"}
               onChange={(e) =>
